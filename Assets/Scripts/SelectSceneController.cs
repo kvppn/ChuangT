162959c48697cde89c7ad0a72c78aa13d220d06a
kvppn <3112716858@qq.com>
2024-05-28 21:44:58 +0800

    123

--- Assets/Scripts/SelectSceneController.cs
+++ Assets/Scripts/SelectSceneController.cs
@@ -8,12 +8,15 @@ public class SelectSceneController : MonoBehaviour
 
     public GameObject selectButon;
     public GameObject selectJieMian;
+    private Animator transmit1;
+    private Animator transmit2;
+    private Animator sleep1;
+    private Animator sleep2;
+    //private bool isCoroutineRunning = false; // 用于标记协程是否正在运行
     // Start is called before the first frame update
-
-   
     void Start()
     {
-        
+        transmit2 = GameObject.FindGameObjectWithTag("2").GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -72,15 +75,42 @@ public class SelectSceneController : MonoBehaviour
     }
     private void OnSceneLoadedclothes(Scene scene, LoadSceneMode mode)
     {
+       
         if (scene.buildIndex == 0)
         {
             Debug.Log("0000022");
             GameObject player = GameObject.FindGameObjectWithTag("player");
             player.transform.position = new Vector3(-0.3f, -4.4f, 0);
-
+            //StartCoroutine(PlayAnimationAndWait());
             SceneManager.sceneLoaded -= OnSceneLoadedclothes;
         }
     }
+   /* IEnumerator PlayAnimationAndWait()
+    {
+        if (transmit2 == null)
+        {
+            yield break; // 如果animator为空，则退出协程
+        }
+
+        transmit2.enabled = true;
+
+        // 等待动画播放完成
+        while (transmit2.GetCurrentAnimatorStateInfo(0).normalizedTime < 1)
+        {
+            yield return null;
+        }
+
+        // 动画播放完成后禁用Animator组件
+        transmit2.enabled = false;
+       // SceneManager.sceneLoaded -= OnSceneLoadedclothes;
+    }
+    void OnDestroy()
+    {
+        if (isCoroutineRunning)
+        {
+            StopAllCoroutines(); // 停止所有协程
+        }
+    }*/
     private void OnSceneLoadedbar(Scene scene, LoadSceneMode mode)
     {
         if (scene.buildIndex == 0)
